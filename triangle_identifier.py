64f1780fad0e77d2c2534803b54aabc4dc114bf1
Laura Terry <proxycharm@gmail.com>
2024-04-03 10:00:20 -0400

    triangle types first commit

---
+++ triangle_identifier.py
@@ -0,0 +1,22 @@
+first_angle = int(input("Please enter the first angle: "))
+second_angle = int(input("Please enter the second angle: "))
+third_angle = int(input("Please enter the second angle: "))
+
+
+if first_angle >=0 and second_angle >=0 and third_angle <= 0:
+    print("Numbers are invalid")
+elif (first_angle + second_angle + third_angle) != 180:
+    print("Numbers are invalid!")
+elif (first_angle or second_angle or third_angle) == 90:
+    print("This is a right triangle")
+elif (first_angle or second_angle or third_angle) > 90:
+    print("This is an obtuse triangle")
+elif (first_angle and second_angle and third_angle) < 90:
+    print("This triangle is acute")
+else:
+    print("I have no idea what you did")
+
+
+#A right triangle has one angle of 90°
+#A obtuse triangle has one angle of more than 90°
+#A triangle is acute if all three angles are less than 90°
