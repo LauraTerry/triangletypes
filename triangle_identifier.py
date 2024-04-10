99b2093d693883b54954b5b038ae5fffdbff422c
Laura Terry <proxycharm@gmail.com>
2024-04-10 12:43:26 -0400

    updated obtuse

--- triangle_identifier.py
+++ triangle_identifier.py
@@ -7,12 +7,12 @@ if first_angle <=0 and second_angle <=0 and third_angle <= 0:
     print("Numbers are invalid")
 elif (first_angle + second_angle + third_angle) != 180:
     print("Numbers are invalid!")
-elif (first_angle and second_angle and third_angle) < 90:
-    print("This triangle is acute")
+elif first_angle >90 or second_angle >90 or third_angle > 90:
+    print("This is an obtuse triangle")
 elif (first_angle or second_angle or third_angle) == 90:
     print("This is a right triangle")
-elif (first_angle or second_angle  or third_angle) > 90:
-    print("This is an obtuse triangle")
+elif (first_angle and second_angle and third_angle) < 90:
+    print("This triangle is acute")
 else:
     print("I have no idea what you did")
 
