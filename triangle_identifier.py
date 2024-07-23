19ba48f1fc001029b005ecb97c85efdf7952c95c
Laura Terry <proxycharm@gmail.com>
2024-07-23 13:55:34 -0400

    updated code for optimal output

--- triangle_identifier.py
+++ triangle_identifier.py
@@ -1,17 +1,20 @@
+print("Welcome to the triangle identifier!")
+
 first_angle = int(input("Please enter the first angle: "))
 second_angle = int(input("Please enter the second angle: "))
 third_angle = int(input("Please enter the third angle: "))
 
-
-if first_angle <=0 and second_angle <=0 and third_angle <= 0:
+#check if input is valid
+if first_angle <=0 or second_angle <=0 or third_angle <= 0:
     print("Numbers are invalid")
 elif (first_angle + second_angle + third_angle) != 180:
-    print("Numbers are invalid!")
-elif first_angle >90 or second_angle >90 or third_angle > 90:
+    print("Numbers are invalid")
+#check for triangle type
+elif first_angle > 90 or second_angle > 90 or third_angle > 90:
     print("This is an obtuse triangle")
-elif (first_angle or second_angle or third_angle) == 90:
+elif first_angle == 90 or second_angle == 90 or third_angle == 90:
     print("This is a right triangle")
-elif (first_angle and second_angle and third_angle) < 90:
+elif first_angle < 90 and second_angle < 90 and third_angle < 90:
     print("This triangle is acute")
 else:
     print("I have no idea what you did")
